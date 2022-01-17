20d30383489b3a3517fc7ab46bc78a4ccc9e8d95
He-Martijn <roert.stapje_0o@icloud.com>
2022-01-17 20:47:31 +0100

    Play one sound with a button ( without errors)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+//import 'package:english_words/english_words.dart';
 import 'package:audioplayers/audioplayers.dart';
 // In the lesson this is something with audiod_cache.dart, but appearantly not anymore :
 // https://github.com/bluefireteam/audioplayers/blob/main/packages/audioplayers/doc/audio_cache.md
 
+// For those of us with an Apple mac m1 :
+// https://stackoverflow.com/questions/69055694/cocoapod-error-tried-to-install-all-the-possible-way-on-m1
 
 void main() => runApp(XylophoneApp());
 
