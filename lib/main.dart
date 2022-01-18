f7fcea01eb77362c5f6ca007cfbf0c630f894f16
He-Martijn <roert.stapje_0o@icloud.com>
2022-01-18 19:49:31 +0100

    Finish the challenge by writing the code for the buttons.

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,58 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNumber){
+      print('playSound is activated and soundNumber = $soundNumber');
+      final player = AudioCache();
+      print('Going to play sound');
+      if (soundNumber>=1 && soundNumber<=7){
+      player.play('note$soundNumber.wav');
+    } else {
+      print('error:');
+      print('soundNumber = $soundNumber');
+      print('Which is out of the range 1 till 7');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Press a button to play a sound'),
+          backgroundColor: Colors.black,
+        ),
         body: SafeArea(
           child: Container(
-            child: Center(
-              child: TextButton(
-                // In the lesson it's a Flatbutton, but that one is depricated.
-                onPressed: (){
-                  final player = AudioCache();
-                  player.play('note1.wav');
-                },
-                child: Text('Click me'),
-
-              ),
-            ),
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.grey.shade900,
+            child: Column( // The colors : red, orange, yellow, green, darkgreen, blue, purple
+              children: [
+                Expanded(
+                  child: TextButton(onPressed: (){playSound(1);},child: Container(color: Colors.red,)),
+                ),
+                Expanded(
+                  child: TextButton(onPressed: (){playSound(2);},child: Container(color: Colors.orange,)),
+                ),
+                Expanded(
+                  child: TextButton(onPressed: (){playSound(3);},child: Container(color: Colors.yellow,)),
+                ),
+                Expanded(
+                  child: TextButton(onPressed: (){playSound(4);},child: Container(color: Colors.green.shade300,)),
+                ),
+                Expanded(
+                  child: TextButton(onPressed: (){playSound(5);},child: Container(color: Colors.green.shade800,)),
+                ),
+                Expanded(
+                  child: TextButton(onPressed: (){playSound(6);},child: Container(color: Colors.blue,)),
+                ),
+                Expanded(
+                  child: TextButton(onPressed: (){playSound(7);},child: Container(color: Colors.purple,)),
+                ),
+              ],
+            )
           ),
         ),
       ),
